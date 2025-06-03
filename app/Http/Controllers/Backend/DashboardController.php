96915d9261ab63b55b1d01564a78bb29e6589bcf
Son Nguyen <nbinhson8@gmail.com>
2025-06-03 12:36:07 +0700

    layout admin

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -13,6 +13,35 @@ class DashboardController extends Controller
 
     public function index()
     {
-        return view('backend.dashboard.index');
+        $config = $this->config();
+        $template = 'backend.dashborad.home.index'
+        return view('backend.dashboard.layout', compact(
+            'template',
+            'config'
+        ));
     }
+
+    private function config(){
+        return [
+            'js' => [
+                '{{ asset('js/plugins/flot/jquery.flot.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.tooltip.min.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.spline.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.resize.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.pie.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.symbol.js') }}',
+                '{{ asset('js/plugins/flot/jquery.flot.time.js') }}',
+                '{{ asset('js/plugins/peity/jquery.peity.min.js') }}',
+                '{{ asset('js/demo/peity-demo.js') }}',
+                '{{ asset('js/inspinia.js') }}',
+                '{{ asset('js/plugins/pace/pace.min.js') }}',
+                '{{ asset('js/plugins/jvectormap/jquery-jvectormap-2.0.2.min.js') }}',
+                '{{ asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}',
+                '{{ asset('js/plugins/easypiechart/jquery.easypiechart.js') }}',
+                '{{ asset('js/plugins/sparkline/jquery.sparkline.min.js') }}',
+                '{{ asset('js/demo/sparkline-demo.js') }}'
+            ]
+            ];
+    }
+
 }
