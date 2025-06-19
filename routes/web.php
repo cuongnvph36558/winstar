9ace289e220912ff1a28bea66373d9629027d016
BuiAnhVan3624 <buianhvan3624@gmail.com>
2025-06-19 21:02:54 +0700

    Login & Register

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,5 @@ Route::prefix('admin')->group(function () {
 // Login 
 Route::get('login', [AuthenticationController::class, 'login'])->name('login');
 Route::post('login', [AuthenticationController::class, 'postLogin'])->name('postLogin');
-Route::get('register', [AuthenticationController::class, 'register'])->name('register');
-Route::post('register', [AuthenticationController::class, 'postRegister'])->name('postRegister');
+
 
