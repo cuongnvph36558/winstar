bb522e0577bbf4606c370f776e8be50a9ef4306e
Khương Nguyễn <Khng Nguyn>
2025-06-25 13:29:29 +0700

    model post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,29 +1,29 @@
 <?php
 
-// namespace App\Models;
+namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Model;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-// class Post extends Model
-// {
-//     use HasFactory;
+class Post extends Model
+{
+    use HasFactory;
 
-//     protected $fillable = [
-//         'author_id',
-//         'title',
-//         'content',
-//         'image',
-//         'status',
-//         'published_at',
-//     ];
+    protected $fillable = [
+        'author_id',
+        'title',
+        'content',
+        'image',
+        'status',
+        'published_at',
+    ];
 
-//     protected $casts = [
-//         'published_at' => 'datetime',
-//     ];
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
 
-//     public function author()
-//     {
-//         return $this->belongsTo(User::class, 'author_id');
-//     }
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
 }
