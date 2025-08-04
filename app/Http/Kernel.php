0ff9de70f5691917de2ecad4a7eb0d6b3316060d
hieuhn2005 <hieutdph49897@gmail.com>
2025-08-04 12:59:15 +0700

    cap nhat kernel middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'email.verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         'role' => \App\Http\Middleware\RoleMiddleware::class,
         'permission' => \App\Http\Middleware\PermissionMiddleware::class,
         'admin.access' => \App\Http\Middleware\AdminAccessMiddleware::class,
