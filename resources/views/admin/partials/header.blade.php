854118c22ac440accd249a1515e9fb070da41760
cuongdev04 <cuongnvph36558@fpt.edu.vn>
2025-08-05 00:37:04 +0700

    cap nhat header admin voi thong bao realtime

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -8,97 +8,28 @@
                 <span class="m-r-sm text-muted welcome-message">Chào mừng đến với hệ thống quản trị WINSTAR.</span>
             </li>
             <li class="dropdown">
-                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                    <i class="fa fa-envelope"></i> <span class="label label-warning">16</span>
+                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#" id="new-orders-notification">
+                    <i class="fa fa-shopping-cart"></i> <span class="label label-warning" id="new-orders-count">0</span>
                 </a>
-                <ul class="dropdown-menu dropdown-messages">
-                    <li>
-                        <div class="dropdown-messages-box">
-                            <a href="profile.html" class="pull-left">
-                                <img alt="image" class="img-circle" src="img/a7.jpg">
-                            </a>
-                            <div class="media-body">
-                                <small class="pull-right">46 giờ trước</small>
-                                <strong>Mike Loreipsum</strong> đã bắt đầu theo dõi <strong>Monica
-                                    Smith</strong>. <br>
-                                <small class="text-muted">3 ngày trước lúc 7:58 tối - 10.06.2014</small>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <div class="dropdown-messages-box">
-                            <a href="profile.html" class="pull-left">
-                                <img alt="image" class="img-circle" src="img/a4.jpg">
-                            </a>
-                            <div class="media-body ">
-                                <small class="pull-right text-navy">5 giờ trước</small>
-                                <strong>Chris Johnatan Overtunk</strong> đã bắt đầu theo dõi <strong>Monica
-                                    Smith</strong>. <br>
-                                <small class="text-muted">Hôm qua 1:21 chiều - 11.06.2014</small>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <div class="dropdown-messages-box">
-                            <a href="profile.html" class="pull-left">
-                                <img alt="image" class="img-circle" src="img/profile.jpg">
-                            </a>
-                            <div class="media-body ">
-                                <small class="pull-right">23 giờ trước</small>
-                                <strong>Monica Smith</strong> thích <strong>Kim Smith</strong>. <br>
-                                <small class="text-muted">2 ngày trước lúc 2:30 sáng - 11.06.2014</small>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="divider"></li>
+                <ul class="dropdown-menu dropdown-messages" id="new-orders-list">
                     <li>
                         <div class="text-center link-block">
-                            <a href="mailbox.html">
-                                <i class="fa fa-envelope"></i> <strong>Xem tất cả tin nhắn</strong>
+                            <a href="{{ route('admin.order.index') }}">
+                                <i class="fa fa-shopping-cart"></i> <strong>Xem tất cả đơn hàng</strong>
                             </a>
                         </div>
                     </li>
                 </ul>
             </li>
             <li class="dropdown">
-                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                    <i class="fa fa-bell"></i> <span class="label label-primary">8</span>
+                <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#" id="new-reviews-notification">
+                    <i class="fa fa-star"></i> <span class="label label-primary" id="new-reviews-count">0</span>
                 </a>
-                <ul class="dropdown-menu dropdown-alerts">
-                    <li>
-                        <a href="mailbox.html">
-                            <div>
-                                <i class="fa fa-envelope fa-fw"></i> Bạn có 16 tin nhắn mới
-                                <span class="pull-right text-muted small">4 phút trước</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="profile.html">
-                            <div>
-                                <i class="fa fa-twitter fa-fw"></i> 3 người theo dõi mới
-                                <span class="pull-right text-muted small">12 phút trước</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="grid_options.html">
-                            <div>
-                                <i class="fa fa-upload fa-fw"></i> Máy chủ đã khởi động lại
-                                <span class="pull-right text-muted small">4 phút trước</span>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="divider"></li>
+                <ul class="dropdown-menu dropdown-alerts" id="new-reviews-list">
                     <li>
                         <div class="text-center link-block">
-                            <a href="notifications.html">
-                                <strong>Xem tất cả thông báo</strong>
-                                <i class="fa fa-angle-right"></i>
+                            <a href="{{ route('admin.reviews.list') }}">
+                                <i class="fa fa-star"></i> <strong>Xem tất cả đánh giá</strong>
                             </a>
                         </div>
                     </li>
